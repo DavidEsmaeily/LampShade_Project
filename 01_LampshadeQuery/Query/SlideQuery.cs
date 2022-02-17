afb85c664cd14bdb270e5a3b8d1eece8efa53f94
DavidEsmaeily <DavoodEsmaeily0049@gmail.com>
2022-02-17 12:52:35 +0330

    Reslove Project Conflict

---
+++ 01_LampshadeQuery/Query/SlideQuery.cs
@@ -0,0 +1,33 @@
+﻿using _01_LampshadeQuery.Contracts.Slide;
+using ShopManagement.Infrastructure.EFCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_LampshadeQuery.Query
+{
+    public class SlideQuery : ISlideQuery
+    {
+        private readonly ShopContext _context;
+
+        public SlideQuery(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public List<SlideQueryModel> GetSlides()
+        {
+            return _context.Slides.Where(x => !x.IsRemoved).Select(x => new SlideQueryModel
+            {
+                Id = x.Id,
+                Heading = x.Heading,
+                Link = x.Link,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                Text = x.Text,
+                Title = x.Title,
+                PictureTitle = x.PictureTitle,
+                BtnText = x.BtnText
+            }).ToList();
+        }
+    }
+}
---
+++ DiscountManagement.Domain/CustomerDiscountAgg/ICustomerDiscountRepository.cs
@@ -0,0 +1,15 @@
+﻿using _0_Framework.Domain;
+
+namespace DiscountManagement.Domain.CustomerDiscountAgg
+{
+    public partial class CustomerDiscount
+    {
+        public interface ICustomerDiscountRepository : IRepository<long , CustomerDiscount>
+        {
+
+        }
+
+    }
+
+    
+}
